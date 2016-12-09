85bcecab7479b8d6bc183083e5cc7c8ae7467c90
Tyler Conlee <tylerlconlee@gmail.com>
2016-12-09 13:06:50 -0800

    added early skeleton for app structure

---
+++ main.go
@@ -0,0 +1,15 @@
+// SummitAPI creates a web server that handles requests made by SummitDaemon
+// Designed to run in parallel in a containerized fashion
+// SummitAPI is required to be running in order to run SummitDaemon
+
+package main
+
+func main() {
+	// Set initial configurations, use overrides from environment variables
+
+	// Use configuration to create a new connection
+
+	// Use connection to be passed to listen for data push/pull requests
+
+	// Listen and serve HTTP requests from the PHP app
+}
